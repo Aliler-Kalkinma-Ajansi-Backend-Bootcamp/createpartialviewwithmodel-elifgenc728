cfff18c034e4afc2042b9d0549b3d7e924f58210
elifgenc728@gmail.com <76621852+elifgenc728@users.noreply.github.com>
2021-01-07 18:51:04 +0300

    ikinci proje

---
+++ AracBilgisi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AracBilgisi.Models
+{
+    public class User
+    {
+        public string markası { get; set; }
+        public string kullanimdurumu { get; set; }
+        public int yili { get; set; }
+        public double fiyati { get; set; }
+    }
+}
